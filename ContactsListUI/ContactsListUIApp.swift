539d68d09e9899b990b371ca501c486e5c2e8009
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-10-19 08:52:19 +0200

    Initial Commit

---
+++ ContactsListUI/ContactsListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsListUIApp.swift
+//  ContactsListUI
+//
+//  Created by user246073 on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ContactsListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
